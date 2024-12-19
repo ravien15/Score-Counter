ffa322d5a3a74e5927b42067b97d6550565efefb
Gamer <Gamer@RAVIEN>
2024-12-19 21:48:54 +0800

    footer fixed

--- footer.php
+++ footer.php
@@ -1,14 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome CSS -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-
-</head>
 <body>
     <!-- Footer Section -->
 <footer class="bg-light text-center text-lg-start mt-5 border-top">
